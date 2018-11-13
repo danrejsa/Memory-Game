dce7a552a7c9d5f2c532e479ea5367cab5fb7cb6
Daniel Christopher <40594492+danrejsa@users.noreply.github.com>
2018-11-13 07:13:32 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,164 +1,176 @@
-let symbols = ['code', 'code', 'bug', 'bug', 'user-secret', 'user-secret', 'terminal', 'terminal', 'globe', 'globe', 'laptop', 'laptop', 'server', 'server', 'power-off', 'power-off'],
-	opened = [],
-	match = 0,
-	Clicks = 0,
-	$Playground = $('.Playground'),
-	$scorePanel = $('#score-panel'),
-	$moveNum = $('.Clicks'),
-	$ratingStars = $('.fa-angellist'),
-	$PlayAgain = $('.PlayAgain'),
-	delay = 400,
-	currentseconds,
-	second = 0,
-	$seconds = $('.seconds'),
-	totalbox = symbols.length / 2,
-	rank3stars = 10,
-	rank2stars = 20,
-	rank1stars = 30;
+const cardList = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
+let started = false;
+let openCards = [];
+let moves = 0;
+let timeCount = 0;
+let solvedCount = 0;
+let timerPtr;
 
+
+// Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-	var currentIndex = array.length, temporaryValue, randomIndex;
-
-	while (0 !== currentIndex) {
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex -= 1;
-		temporaryValue = array[currentIndex];
-		array[currentIndex] = array[randomIndex];
-		array[randomIndex] = temporaryValue;
-	}
-
-	return array;
+    let currentIndex = array.length, temporaryValue, randomIndex;
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
 }
 
-
-function initGame() {
-	var boxes = shuffle(symbols);
-	$Playground.empty();
-	match = 0;
-	Clicks = 0;
-	$moveNum.text('0');
-	$ratingStars.removeClass('fa-thumbs-down').addClass('fa-star');
-	for (var i = 0; i < boxes.length; i++) {
-		$Playground.append($('<li class="box"><i class="fa fa-' + boxes[i] + '"></i></li>'))
-	}
-	addboxListener();
-
-	resetseconds(currentseconds);
-	second = 0;
-	$seconds.text(`${second}`)
-	initTime();
-};
-
-
-function setRating(moves) {
-	var rating = 3;
-	if (moves > rank3stars && moves < rank2stars) {
-		$ratingStars.eq(2).removeClass('fa-angellist').addClass('fa-thumbs-down');
-		rating = 2;
-	} else if (moves > rank2stars && moves < rank1stars) {
-		$ratingStars.eq(1).removeClass('fa-angellist').addClass('fa-thumbs-down');
-		rating = 1;
-	} else if (moves > rank1stars) {
-		$ratingStars.eq(0).removeClass('fa-angellist').addClass('fa-thumbs-down');
-		rating = 0;
-	}
-	return { score: rating };
-};
-
-
-
-function endGame(Clicks, score) {
-	swal({
-		allowEscapeKey: false,
-		allowOutsideClick: false,
-		title: 'Well Done!',
-		text: 'With ' + Clicks + ' Clicks and ' + score + ' Stars in ' + second + ' Seconds.',
-		type: 'success',
-		confirmButtonColor: '#02ccba',
-		confirmButtonText: 'Play again!'
-	}).then(function (isConfirm) {
-		if (isConfirm) {
-			initGame();
-		}
-	})
+// get class value from card DOM
+function getClassFromCard(card){
+    return card[0].firstChild.className;
 }
 
+// check open cards when count = 2
+function checkOpenCards(){
+    if (getClassFromCard(openCards[0]) === getClassFromCard(openCards[1])){
+        solvedCount++;
+        openCards.forEach(function(card){
+            card.animateCss('tada', function(){
+                card.toggleClass("open show match");
+            });
+        });
+    } else {
+        openCards.forEach(function(card){
+            card.animateCss('shake', function(){
+                card.toggleClass("open show");
+            });
+        });
+    }
+    openCards = [];
+    incrementMove();
+    if (solvedCount === 8){
+        endGame();
+    }
+}
 
-$PlayAgain.bind('click', function () {
-	swal({
-		allowEscapeKey: false,
-		allowOutsideClick: false,
-		title: 'Game Over',		
-		type: 'warning',
-		showCancelButton: false,
-		confirmButtonColor: '#02ccba',
-		cancelButtonColor: '#f95c3c',
-		confirmButtonText: 'Play Again',
-	}).then(function (isConfirm) {
-		if (isConfirm) {
-			initGame();
-		}
-	})
+// starts the timer
+function startTimer(){
+    timeCount += 1;
+    $("#timer").html(timeCount);
+    timerPtr = setTimeout(startTimer, 1000);
+}
+
+// increment move count
+function incrementMove(){
+    moves += 1;
+    $("#moves").html(moves);
+    if (moves === 14 || moves === 20){
+        reduceStar();
+    }
+}
+
+// event handler for when the card is clicked
+function cardClick(event){
+    // check opened or matched card
+    let classes = $(this).attr("class");
+    if (classes.search('open') * classes.search('match') !== 1){
+        // both should be -1
+        return;
+    }
+    // start game if needed
+    if (!started) {
+        started = true;
+        timeCount = 0;
+        timerPtr = setTimeout(startTimer, 1000);
+    }
+    // cards can be flipped
+    if (openCards.length < 2){
+        $(this).toggleClass("open show");
+        openCards.push($(this));
+    }
+    // check if cards match
+    if (openCards.length === 2){
+        checkOpenCards();
+    }
+}
+
+// create individual card element
+function createCard(cardClass){
+    $("ul.deck").append(`<li class="card"><i class="fa ${cardClass}"></i></li>`);
+}
+
+// populate cards in DOM
+function populateCards(){
+    shuffle(cardList.concat(cardList)).forEach(createCard);
+}
+
+// reset game
+function resetGame(){
+    $("ul.deck").html("");
+    $(".stars").html("");
+    moves = -1;
+    incrementMove();
+    started = false;
+    openCards = [];
+    timeCount = 0;
+    solvedCount = 0;
+    clearTimeout(timerPtr);
+    $("#timer").html(0);
+    // re-setup game
+    initGame();
+}
+
+// runs when game has been won
+function endGame(){
+    // stop timer
+    clearTimeout(timerPtr);
+    // show prompt
+    let stars = $(".fa-star").length;
+    vex.dialog.confirm({
+        message: `Congrats! You just won the game in ${timeCount} seconds with ${stars}/3 star rating. Do you want to play again?`,
+        callback: function(value){
+            if (value){
+                resetGame();
+            }
+        }
+    });
+}
+
+// initialize stars display
+function initStars(){
+    for (let i=0; i<3; i++){
+        $(".stars").append(`<li><i class="fa fa-star"></i></li>`);
+    }
+}
+
+// reduce star rating
+function reduceStar(){
+    let stars = $(".fa-star");
+    $(stars[stars.length-1]).toggleClass("fa-star fa-star-o");
+}
+
+// init game
+function initGame(){
+    populateCards();
+    initStars();
+    $(".card").click(cardClick);
+}
+
+// things done after DOM is loaded for the first time
+$(document).ready(function(){
+    initGame();
+    $("#restart").click(resetGame);
+    vex.defaultOptions.className = 'vex-theme-os';
+    vex.dialog.buttons.YES.text = 'Yes!';
+    vex.dialog.buttons.NO.text = 'No';
 });
 
-var addboxListener = function () {
-
-	
-	$Playground.find('.box').bind('click', function () {
-		var $this = $(this)
-
-		if ($this.hasClass('show') || $this.hasClass('match')) { return true; }
-
-		var box = $this.context.innerHTML;
-		$this.addClass('open show');
-		opened.push(box);
-
-		
-		if (opened.length > 1) {
-			if (box === opened[0]) {
-				$Playground.find('.open').addClass('match animated infinite rubberBand');
-				setTimeout(function () {
-					$Playground.find('.match').removeClass('open show animated infinite rubberBand');
-				}, delay);
-				match++;
-			} else {
-				$Playground.find('.open').addClass('notmatch animated infinite wobble');
-				setTimeout(function () {
-					$Playground.find('.open').removeClass('animated infinite wobble');
-				}, delay / 1.5);
-				setTimeout(function () {
-					$Playground.find('.open').removeClass('open show notmatch animated infinite wobble');
-				}, delay);
-			}
-			opened = [];
-			Clicks++;
-			setRating(Clicks);
-			$moveNum.html(Clicks);
-		}
-
-		
-		if (totalbox === match) {
-			setRating(Clicks);
-			var score = setRating(Clicks).score;
-			setTimeout(function () {
-				endGame(Clicks, score);
-			}, 500);
-		}
-	});
-};
-
-
-function initTime() {
-	currentseconds = setInterval(function () {
-		$seconds.text(`${second}`)
-		second = second + 1
-	}, 1000);
-}
-
-function resetseconds(seconds) {
-	if (seconds) {
-		clearInterval(seconds);
-	}
-}
-
-initGame();
+// load animateCss
+// taken from https://github.com/daneden/animate.css/#usage
+$.fn.extend({
+    animateCss: function (animationName, callback) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function () {
+            $(this).removeClass('animated ' + animationName);
+            if (callback) {
+                callback();
+            }
+        });
+        return this;
+    }
+});
