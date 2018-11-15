096f6b5352a010b0a40b3d183519c4bb5d3f7856
Daniel Christopher <40594492+danrejsa@users.noreply.github.com>
2018-11-15 00:39:47 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -126,7 +126,7 @@ function endGame(){
     // show prompt
     let stars = $(".fa-star").length;
     vex.dialog.confirm({
-        message: `Congrats! You just won the game in ${timeCount} seconds with ${stars}/3 star rating. Do you want to play again?`,
+        message: `Good Job! You finished in ${timeCount} seconds with ${stars}/3 star rating`,
         callback: function(value){
             if (value){
                 resetGame();
@@ -160,8 +160,8 @@ $(document).ready(function(){
     initGame();
     $("#restart").click(resetGame);
     vex.defaultOptions.className = 'vex-theme-os';
-    vex.dialog.buttons.YES.text = 'Yes!';
-    vex.dialog.buttons.NO.text = 'No';
+    vex.dialog.buttons.YES.text = 'Play Again';
+    vex.dialog.buttons.NO.text = 'Cancel';
 });
 
 // load animateCss
